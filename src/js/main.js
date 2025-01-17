50e40d097fce4709b512e1ab63837f1b86675178
dsdjulia <jag0057@alu.medac.es>
2025-01-18 00:12:35 +0100

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,1 @@
-// Importa el CSS personalizado con SASS
 import '../scss/styles.scss'
-
-// Importa todo el JavaScript de Bootstrap (lo comento para el despliegue)
-// import * as bootstrap from 'bootstrap'
